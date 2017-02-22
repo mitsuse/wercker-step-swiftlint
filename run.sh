39f1ff64a577d6c28cc2a011be42b9eb789a4382
Tomoya Kose <tomoya@mitsuse.jp>
2017-02-22 22:07:40 +0900

    Use the correct path of sources.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 temporary_directory=$(mktemp -d)
 cache_directory=${WERCKER_CACHE_DIR}/mitsuse/swiftlint/${WERCKER_SWIFTLINT_VERSION}
 swiftlint=${cache_directory}/release/swiftlint
-config_path=${WERCKER_GIT_REPOSITORY}/${WERCKER_SWIFTLINT_CONFIG}
+config_path=${WERCKER_SOURCE_DIR}/${WERCKER_SWIFTLINT_CONFIG}
 
 if [ ! -f "${swiftlint}" ]; then
     git clone https://github.com/realm/SwiftLint.git \
@@ -13,5 +13,5 @@ if [ ! -f "${swiftlint}" ]; then
     swift build -c release --chdir ${temporary_directory} --build-path ${cache_directory}
 fi
 
-$swiftlint lint --strict --config ${config_path} --path ${WERCKER_GIT_REPOSITORY}/Sources
-$swiftlint lint --strict --config ${config_path} --path ${WERCKER_GIT_REPOSITORY}/Tests
+$swiftlint lint --strict --config ${config_path} --path ${WERCKER_SOURCE_DIR}/Sources
+$swiftlint lint --strict --config ${config_path} --path ${WERCKER_SOURCE_DIR}/Tests
