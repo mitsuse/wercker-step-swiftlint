280cffa62871332c5b74d7d9b4a8464bdd7c85b7
Tomoya Kose <tomoya@mitsuse.jp>
2017-02-22 21:55:55 +0900

    Fix caching.

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 temporary_directory=$(mktemp -d)
-cache_directory=${WERCKER_CACHE_DIR}/mitsuse/swiftlint
-swiftlint=${cache_directory}/swiftlint-${WERCKER_SWIFTLINT_VERSION}
+cache_directory=${WERCKER_CACHE_DIR}/mitsuse/swiftlint/${WERCKER_SWIFTLINT_VERSION}
+swiftlint=${cache_directory}/release/swiftlint
 config_path=${WERCKER_GIT_REPOSITORY}/${WERCKER_SWIFTLINT_CONFIG}
 
 if [ ! -f "${swiftlint}" ]; then
@@ -10,9 +10,7 @@ if [ ! -f "${swiftlint}" ]; then
         --branch ${WERCKER_SWIFTLINT_VERSION} \
         --depth 1 \
         ${temporary_directory}
-    swift build -c release -C ${temporary_directory}
-    mkdir -p $cache_directory
-    cp ${temporary_directory}/.build/release/swiftlint ${swiftlint}
+    swift build -c release --chdir ${temporary_directory} --build-path ${cache_directory}
 fi
 
 $swiftlint lint --strict --config ${config_path} --path ${WERCKER_GIT_REPOSITORY}/Sources
